77b002fd0ff14b6acc972e1943e66cc097b7f415
sakhsi chourasiya <sakshi.chourasiya73899@gmail.com>
2026-07-06 02:58:14 +0530

    Frontend Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { ToastContainer } from "react-toastify";
@@ -6,7 +5,6 @@ import { useDispatch } from "react-redux";
 import { Loader } from "lucide-react";
 import { useEffect } from "react";
 import { getUser } from "./store/slices/authSlice";
-
 
 // Layout Components
 import Navbar from "./components/Layout/Navbar";
@@ -93,73 +91,5 @@ const App = () => {
     </>
   );
 };
-=======
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import SideBar from "./components/SideBar";
-import Login from "./pages/Login";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
-import { ToastContainer } from "react-toastify";
-import { useSelector } from "react-redux";
-import Dashboard from "./components/Dashboard";
-import Products from "./components/Products";
-import Orders from "./components/Orders";
-import Profile from "./components/Profile";
-import Users from "./components/Users";
-
-
-
-function App() {
-  const { openedComponent } = useSelector(state => state.extra)
-  const { user, isAuthenticated } = useSelector(state => state.auth)
-  const renderDashboardContent = () => {
-    switch (openedComponent) {
-      case "Dashboard":
-        return <Dashboard />;
-      case "Products":
-        return <Products />;
-      case "Orders":
-        return <Orders />;
-      case "Profile":
-        return <Profile />;
-      case "Users":
-        return <Users />;
-
-      default:
-        return <Dashboard />;
-    }
-  }
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/password/forgot" element={<ForgotPassword />} />
-        <Route path="/password/reset/:token" element={<ResetPassword />} />
-
-        {/* Protected Admin Route */}
-        <Route
-          path="/"
-          element={
-            isAuthenticated && user?.role === "Admin" ? (
-              <div className="flex min-h-screen">
-                <SideBar />
-                {renderDashboardContent()}
-              </div>
-            ) : (
-              <Navigate to="/login" replace />
-            )
-          }
-        />
-      </Routes>
-      <ToastContainer theme="dark" />
-    </Router>
-  );
-}
->>>>>>> 701e9bf349e789c5991dcb89f0a7b70b67dca075
 
 export default App;
