b58889e4d5e073c362951c2c286d6f888632c57d
sakhsi chourasiya <sakshi.chourasiya73899@gmail.com>
2026-07-06 09:50:33 +0530

    Frontend Updated

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "/",
+      : "https://followmerce-3.onrender.com/api",
   withCredentials: true,
 });
